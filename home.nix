420b2d25a2d373ccc5ab988e25a836ce174a46a8
akhlus <samuellarcombe@gmail.com>
2025-03-24 05:53:00 +0000

    update

--- home.nix
+++ home.nix
@@ -46,6 +46,8 @@
     Environment="PATH=%h/.nix-profile/bin:/usr/local/sbin:/usr/local/bin:/usr/local/games:/usr/sbin:/usr/bin:/usr/games:/sbin:/bin"
     Environment="XDG_DATA_DIRS=%h/.nix-profile/share:%h/.local/share:%h/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share"
   '';
+  
 
   nixpkgs.config.allowUnfree = true;
+  home.stateVersion = "24.11";
 }
