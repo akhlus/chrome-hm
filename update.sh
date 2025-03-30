b10cd8becd3aa0b0351878536a54ea972b944f0d
akhlus <samuellarcombe@gmail.com>
2025-03-30 17:32:40 +0100

    changed update script

--- update.sh
+++ update.sh
@@ -1,38 +1,47 @@
 #!/usr/bin/env bash
 
-# Default values
 FLAKE_PATH="${FLAKE_PATH:-.}"
 MODE="switch"
+FORMAT="true"
 
-# Parse arguments
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -p|--path) FLAKE_PATH="$2"; shift ;;
         -m|--mode) MODE="$2"; shift ;;
+        -f|--format) FORMAT="$2"; shift ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
 done
 
-
-alejandra "$FLAKE_PATH"
-
-git -C "$FLAKE_PATH" diff
-
-nix flake update "$FLAKE_PATH"
-
-    if ! home-manager "$MODE" --flake "$FLAKE_PATH#home" &> nixos-switch.log; then
-        grep --color error nixos-switch.log >&2
-        exit 1
+if git -C "$FLAKE_PATH" diff --quiet; then
+    read -p "No changes detected - proceed anyway? [y]/n" cont
+    cont=${cont:-y}  # Default to 'y'
+    if [[ "$cont" == "n" ]]; then
+        exit 0
     fi
+fi
+
+if [[ "$format" == "true" ]]; then
+    alejandra "$FLAKE_PATH"
+fi
 
 git -C "$FLAKE_PATH" add .
 
+git -C "$FLAKE_PATH" diff --staged -U0
 
+nix flake update "$FLAKE_PATH"
+
+if ! home-manager "$MODE" --flake "$FLAKE_PATH#home" &> update.log; then
+    grep --color error update.log >&2
+    exit 1
+fi
+
+date=$(date '+%Y-%m-%d-%H-%M')
 read -p "Do you want to commit the changes? (y/[n]): " commit_choice
 if [[ "$commit_choice" == "y" ]]; then
     read -p "Enter commit message: " commit_msg
-    git -C "$FLAKE_PATH" commit -m "$commit_msg"
+    git -C "$FLAKE_PATH" commit -am "$date $commit_msg"
 else
     exit 0
 fi
